4e89edd931cfdc275ceb912ea7e2ec689f4cb6f2
Gabriel-Gomes-Amorim <gomez.amorim18@hotmail.com>
2024-02-22 13:15:25 -0300

    ajustando variavel de ambiente

--- web/vite.config.js
+++ web/vite.config.js
@@ -6,13 +6,20 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
+  preview: {
+    port: 3333,
+    strictPort: true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 3333,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:3333'
   }
 })
