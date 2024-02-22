9f260307568a74a81ab42b84295da3bc4f90fe08
Gabriel-Gomes-Amorim <gomez.amorim18@hotmail.com>
2024-02-22 12:40:05 -0300

    alterando titulo

--- web/src/components/ListaFornecedores.vue
+++ web/src/components/ListaFornecedores.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2 class="text-center mt-lg-5 mb-5">Fornecedores</h2>
+    <h2 class="text-center mt-lg-5 mb-5">Lista de Fornecedores</h2>
     <div class="d-flex justify-content-center">
       <div class="col-lg-8">
         <!-- Filtro -->
